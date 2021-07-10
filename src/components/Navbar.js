f13c1033d4343b5762f1c1baaf0ddba5b82c4034
Bruno Gomes <brunog.vix@gmail.com>
2021-07-09 23:29:35 -0300

    criando o Menu de rotas

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+
+
 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        <Nav>
+            <Logo>Bio Clima Engenharia</Logo>
+            <MenuBars />
+            <NavMenu>
+
+            </NavMenu>
+            <h1>Aqui é o nav</h1>
+        </Nav>
     )
 }
+
+const Nav = styled.nav`
+    height: 60px;
+    background: red;
+`;
+const Logo = styled(Link)`
+    color: #fff;
+`;
+
+const MenuBars = styled.i`
+
+`;
+
+const NavMenu = styled.div`
+
+`;
+
 
 export default Navbar;
---
+++ src/data/MenuData.js
@@ -0,0 +1,5 @@
+export const menuData = [
+    {title: 'Sobre', link: '/about'},
+    {title: 'Serviços', link: '/services'},
+    {title: 'Preços', link: '/price'}
+];
