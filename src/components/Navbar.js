a6832170b1719ac60e96784cc6213e5282fe07b5
Bruno Gomes <brunog.vix@gmail.com>
2021-07-22 16:05:06 -0300

    mudado o botão

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,18 +4,20 @@ import { Link } from 'react-router-dom';
 import { menuData } from '../data/MenuData';
 import { Button } from './Button';
 import { FaBars } from 'react-icons/fa'
-
+import logo from '../assets/BioClima.svg'
 
 const Navbar = ({ toggle }) => {
 
-    function raiseOnClick(){
+    function raiseOnClick() {
         const url = 'https://api.whatsapp.com/send?phone=5527997871146&text=Ol%C3%A1%2C%20gostaria%20de%20entrar%20em%20contato%20com%20voc%C3%AAs.%20';
         window.open(url, '_blank');
     }
 
     return (
         <Nav>
-            <Logo to="/">Bio Clima Engenharia</Logo>
+            <Logo to="/">
+                <img src={logo} alt="Bio Clima Engenharia" />
+            </Logo>
             <MenuBars onClick={toggle} />
             <NavMenu>
                 {menuData.map((item, index) => (
@@ -55,6 +57,9 @@ const NavLink = css`
 const Logo = styled(Link)`
     ${NavLink}
     font-size: 24px;
+    img{
+        width: 50%;
+    }
 `;
 
 const MenuBars = styled(FaBars)`
@@ -75,7 +80,7 @@ const MenuBars = styled(FaBars)`
 const NavMenu = styled.div`
     display: flex;
     align-items: center;
-    margin-right: -48px;
+    /* margin-right: -48px; */
 
     @media screen and (max-width: 768px){
         display: none;
